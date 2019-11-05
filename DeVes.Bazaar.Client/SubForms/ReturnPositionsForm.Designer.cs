ed0461b184329b5d84a61395e79fa15c077d2501
Sven Reichert <Sven.Reichert@generic.de>
2019-11-05 22:49:05 +0100

    Scaling fix for TitelImages

--- DeVes.Bazaar.Client/SubForms/ReturnPositionsForm.Designer.cs
+++ DeVes.Bazaar.Client/SubForms/ReturnPositionsForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.bkGroupBox1 = new DeVes.Bazaar.Client.CustControls.BkGroupBox();
+            this.m_lbErrorMsg = new System.Windows.Forms.Label();
             this.dvTextBox1 = new DeVes.Bazaar.Client.CustControls.DvTextBox();
             this.bkGroupBox2 = new DeVes.Bazaar.Client.CustControls.BkGroupBox();
             this.m_matlPosLv = new System.Windows.Forms.ListView();
@@ -40,7 +41,6 @@
             this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.button1 = new System.Windows.Forms.Button();
             this.m_confirmAllBtn = new System.Windows.Forms.Button();
-            this.m_lbErrorMsg = new System.Windows.Forms.Label();
             this.bkGroupBox1.SuspendLayout();
             this.bkGroupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -52,12 +52,28 @@
             this.bkGroupBox1.Controls.Add(this.m_lbErrorMsg);
             this.bkGroupBox1.Controls.Add(this.dvTextBox1);
             this.bkGroupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bkGroupBox1.Location = new System.Drawing.Point(5, 5);
+            this.bkGroupBox1.Location = new System.Drawing.Point(13, 12);
+            this.bkGroupBox1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.bkGroupBox1.Name = "bkGroupBox1";
-            this.bkGroupBox1.Size = new System.Drawing.Size(927, 178);
+            this.bkGroupBox1.Padding = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.bkGroupBox1.Size = new System.Drawing.Size(2472, 424);
             this.bkGroupBox1.TabIndex = 0;
             this.bkGroupBox1.TabStop = false;
             this.bkGroupBox1.Text = "Positionseingabe";
+            // 
+            // m_lbErrorMsg
+            // 
+            this.m_lbErrorMsg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.m_lbErrorMsg.BackColor = System.Drawing.Color.Transparent;
+            this.m_lbErrorMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_lbErrorMsg.Location = new System.Drawing.Point(37, 188);
+            this.m_lbErrorMsg.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.m_lbErrorMsg.Name = "m_lbErrorMsg";
+            this.m_lbErrorMsg.Size = new System.Drawing.Size(2408, 203);
+            this.m_lbErrorMsg.TabIndex = 2;
+            this.m_lbErrorMsg.Text = "Positionsnummer existiert bereits!";
+            this.m_lbErrorMsg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // dvTextBox1
             // 
@@ -66,10 +82,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dvTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dvTextBox1.IsMargin = false;
-            this.dvTextBox1.Location = new System.Drawing.Point(12, 21);
+            this.dvTextBox1.Location = new System.Drawing.Point(32, 50);
+            this.dvTextBox1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.dvTextBox1.Name = "dvTextBox1";
             this.dvTextBox1.ResultType = DeVes.Bazaar.Client.CustControls.DvTextBox.ResultTypes.String;
-            this.dvTextBox1.Size = new System.Drawing.Size(905, 53);
+            this.dvTextBox1.Size = new System.Drawing.Size(2407, 121);
             this.dvTextBox1.TabIndex = 0;
             this.dvTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.dvTextBox1.TextChanged += new System.EventHandler(this.dvTextBox1_TextChanged);
@@ -82,9 +99,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.bkGroupBox2.Controls.Add(this.m_matlPosLv);
             this.bkGroupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
-            this.bkGroupBox2.Location = new System.Drawing.Point(5, 189);
+            this.bkGroupBox2.Location = new System.Drawing.Point(13, 451);
+            this.bkGroupBox2.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.bkGroupBox2.Name = "bkGroupBox2";
-            this.bkGroupBox2.Size = new System.Drawing.Size(927, 322);
+            this.bkGroupBox2.Padding = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.bkGroupBox2.Size = new System.Drawing.Size(2472, 768);
             this.bkGroupBox2.TabIndex = 1;
             this.bkGroupBox2.TabStop = false;
             this.bkGroupBox2.Text = "Noch nicht verkaufte Positionen";
@@ -106,10 +125,11 @@
             this.m_matlPosLv.GridLines = true;
             this.m_matlPosLv.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.m_matlPosLv.HideSelection = false;
-            this.m_matlPosLv.Location = new System.Drawing.Point(7, 21);
+            this.m_matlPosLv.Location = new System.Drawing.Point(19, 50);
+            this.m_matlPosLv.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.m_matlPosLv.Name = "m_matlPosLv";
             this.m_matlPosLv.ShowGroups = false;
-            this.m_matlPosLv.Size = new System.Drawing.Size(910, 292);
+            this.m_matlPosLv.Size = new System.Drawing.Size(2420, 691);
             this.m_matlPosLv.TabIndex = 18;
             this.m_matlPosLv.UseCompatibleStateImageBehavior = false;
             this.m_matlPosLv.View = System.Windows.Forms.View.Details;
@@ -151,9 +171,10 @@
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Image = global::DeVes.Bazaar.Client.Properties.Resources.delete2_32x32;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(772, 517);
+            this.button1.Location = new System.Drawing.Point(2059, 1233);
+            this.button1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 59);
+            this.button1.Size = new System.Drawing.Size(427, 141);
             this.button1.TabIndex = 2;
             this.button1.Text = "Schließen";
             this.button1.UseVisualStyleBackColor = true;
@@ -165,38 +186,28 @@
             this.m_confirmAllBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.m_confirmAllBtn.Image = global::DeVes.Bazaar.Client.Properties.Resources.handshake_32x32;
             this.m_confirmAllBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.m_confirmAllBtn.Location = new System.Drawing.Point(12, 517);
+            this.m_confirmAllBtn.Location = new System.Drawing.Point(32, 1233);
+            this.m_confirmAllBtn.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.m_confirmAllBtn.Name = "m_confirmAllBtn";
-            this.m_confirmAllBtn.Size = new System.Drawing.Size(160, 59);
+            this.m_confirmAllBtn.Size = new System.Drawing.Size(427, 141);
             this.m_confirmAllBtn.TabIndex = 3;
             this.m_confirmAllBtn.Text = "Alle bestätigen";
             this.m_confirmAllBtn.UseVisualStyleBackColor = true;
             this.m_confirmAllBtn.Click += new System.EventHandler(this.m_confirmAllBtn_Click);
             // 
-            // m_lbErrorMsg
-            // 
-            this.m_lbErrorMsg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.m_lbErrorMsg.BackColor = System.Drawing.Color.Transparent;
-            this.m_lbErrorMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.m_lbErrorMsg.Location = new System.Drawing.Point(14, 79);
-            this.m_lbErrorMsg.Name = "m_lbErrorMsg";
-            this.m_lbErrorMsg.Size = new System.Drawing.Size(903, 85);
-            this.m_lbErrorMsg.TabIndex = 2;
-            this.m_lbErrorMsg.Text = "Positionsnummer existiert bereits!";
-            this.m_lbErrorMsg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // ReturnPositionsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(939, 582);
+            this.ClientSize = new System.Drawing.Size(2504, 1388);
             this.Controls.Add(this.m_confirmAllBtn);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.bkGroupBox2);
             this.Controls.Add(this.bkGroupBox1);
+            this.Margin = new System.Windows.Forms.Padding(21, 17, 21, 17);
             this.Name = "ReturnPositionsForm";
             this.Text = "Rückgabenerfassung";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.ImportPositionForm_Load);
             this.bkGroupBox1.ResumeLayout(false);
             this.bkGroupBox1.PerformLayout();
